b21908f49493950086e69a618218fa6b7d8d4aa9
bianca.costea <bianca@email.com>
2016-12-15 12:23:36 +0200

    #4 Installation profile

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Defines local settings. 
+ */
+
+$databases['default']['default'] = array (
+  'database' => 'drupal_community_cluj',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
