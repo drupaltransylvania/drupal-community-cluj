bc03cdc9d0bad17cbac585e5a982296ebabfbf64
Hangea Cristian <cristian.hangea@pitechnologies.ro>
2017-04-26 14:27:45 +0300

    Configure Content moderation Flow. See #247.

---
+++ docroot/modules/contrib/workbench/src/Plugin/Block/WorkbenchBlock.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\workbench\Plugin\Block;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Register a block that other modules may hook into.
+ *
+ * @Block(
+ *   id = "workbench_block",
+ *   admin_label = @Translation("Workbench information")
+ * )
+ */
+class WorkbenchBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $items = \Drupal::moduleHandler()->invokeAll('workbench_block');
+    if (empty($items)) {
+      return array();
+    }
+    return array(
+      '#markup' => '<div class="workbench-info-block">' . implode('<br />', $items) . '</div>',
+      '#attached' => array(
+        'library' => array('workbench/workbench.block'),
+      ),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account) {
+    return AccessResult::allowedIfHasPermission($account, 'access workbench');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return ['url.path'];
+  }
+
+}
