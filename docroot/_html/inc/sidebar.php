635a7244133a963db48855e8c27e9a6e2af99b33
Daniel Popdan <dpopdan@pitechnologies.ro>
2017-06-19 18:27:24 +0300

    Separate the homepage template and update the logo and main menu. See DCC-259.

---
+++ docroot/_html/inc/sidebar.php
@@ -0,0 +1,96 @@
+<!-- Sidebar -->
+<div id="sidebar">
+  <div class="inner">
+
+    <!-- Search -->
+    <section id="search" class="alt">
+      <form method="post" action="#">
+        <input type="text" name="query" id="query"
+               placeholder="Search"/>
+      </form>
+    </section>
+
+    <!-- Menu -->
+    <nav id="menu">
+      <header class="major">
+        <h2>Menu</h2>
+      </header>
+      <ul>
+        <li><a href="#">Global Training Days</a></li>
+        <li><a href="#">Events</a></li>
+        <li>
+          <span class="opener">Articles</span>
+          <ul>
+            <li><a href="#">Community</a></li>
+            <li><a href="#">Tehnical</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Users</a></li>
+        <li><a href="#">Forum</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+
+    <!-- Section -->
+    <section>
+      <header class="major">
+        <h2>Bookmark</h2>
+      </header>
+      <div class="mini-posts">
+        <article>
+          <a href="#" class="image"><img
+              src="../images/pic07.jpg"
+              alt=""/></a>
+          <p>Aenean ornare velit lacus, ac varius enim lorem
+            ullamcorper dolore aliquam.</p>
+        </article>
+        <article>
+          <a href="#" class="image"><img
+              src="../images/pic08.jpg"
+              alt=""/></a>
+          <p>Aenean ornare velit lacus, ac varius enim lorem
+            ullamcorper dolore aliquam.</p>
+        </article>
+        <article>
+          <a href="#" class="image"><img
+              src="../images/pic09.jpg"
+              alt=""/></a>
+          <p>Aenean ornare velit lacus, ac varius enim lorem
+            ullamcorper dolore aliquam.</p>
+        </article>
+      </div>
+      <ul class="actions">
+        <li><a href="#" class="button">More</a></li>
+      </ul>
+    </section>
+
+    <!-- Section -->
+    <section>
+      <header class="major">
+        <h2>Get in touch</h2>
+      </header>
+      <p>Sed varius enim lorem ullamcorper dolore aliquam aenean
+        ornare velit lacus, ac varius enim lorem ullamcorper
+        dolore.
+        Proin sed aliquam facilisis ante interdum. Sed nulla
+        amet
+        lorem feugiat tempus aliquam.</p>
+      <ul class="contact">
+        <li class="fa-envelope-o"><a href="#">information@untitled.tld</a>
+        </li>
+        <li class="fa-phone">(000) 000-0000</li>
+        <li class="fa-home">1234 Somewhere Road #8254<br/>
+          Nashville, TN 00000-0000
+        </li>
+      </ul>
+    </section>
+
+    <!-- Footer -->
+    <footer id="footer">
+      <p class="copyright">&copy; Drupal Community Cluj-Napoca. All rights
+        reserved.</p>
+    </footer>
+
+  </div>
+</div>
